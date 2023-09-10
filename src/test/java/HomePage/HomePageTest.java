3970028fd1cf419220e3368e26b4c512c92fdc09
BMS <basel.mohe.ps@gmail.com>
2023-09-10 19:43:53 +0300

    test2

--- src/test/java/HomePage/HomePageTest.java
+++ src/test/java/HomePage/HomePageTest.java
@@ -1,3 +1,5 @@
+package HomePage;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
@@ -8,10 +10,13 @@ import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import static java.lang.Thread.sleep;
+
 public class HomePageTest {
 
     WebDriver driver;
     //WebElement contactButton;
+    WebElement bookingButton;
     JavascriptExecutor jse;
     @BeforeClass
     public void setUp(){
@@ -26,13 +31,20 @@ public class HomePageTest {
 
     @Test(priority = 1)
     public void bookingButtonIsDisplayed()  {
-        WebElement bookingButton=driver.findElement(By.xpath("//*[@id=\"heroText\"]/div/div[3]/a"));
+         bookingButton=driver.findElement(By.xpath("//*[@id=\"heroText\"]/div/div[3]/a"));
         Assert.assertTrue(bookingButton.isDisplayed());
     }
 
-    @Test(priority = 1)
+    @Test(priority = 2)
     public void bookingButtonTextIsCorrect()  {
-        WebElement bookingButton=driver.findElement(By.xpath("//*[@id=\"heroText\"]/div/div[3]/a"));
+         bookingButton=driver.findElement(By.xpath("//*[@id=\"heroText\"]/div/div[3]/a"));
         Assert.assertEquals(bookingButton.getText(),"احجز جلستك الأولى");
+    }
+
+    @Test(priority = 3)
+    public void bookingButtonIsClick() throws InterruptedException {
+        bookingButton=driver.findElement(By.xpath("//*[@id=\"heroText\"]/div/div[3]/a"));
+        bookingButton.click();
+        sleep(2000);
     }
 }
